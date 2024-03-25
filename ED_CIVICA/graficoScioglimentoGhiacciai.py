ecd6e2dd4dad159db7f6976eba3e9cd52cbc4966
gabryBussone <144807730+gabryBussone@users.noreply.github.com>
2024-03-25 11:20:22 +0100

    Update graficoScioglimentoGhiacciai.py

--- ED_CIVICA/graficoScioglimentoGhiacciai.py
+++ ED_CIVICA/graficoScioglimentoGhiacciai.py
@@ -1,22 +1,21 @@
 import csv
 import matplotlib.pyplot as plt
 
+anni = []
+variazione_massa_cumulativa = []
 
-years = []
-cumulative_mass_variation = []
-
-# lettura dei dati dal file CSV
+# Lettura dei dati dal file CSV
 with open('datiGhiacciai.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        year = int(row[0])
-        mass_variation = int(row[3])  # considero solo la variazione cumulativa della massa
-        years.append(year)
-        cumulative_mass_variation.append(mass_variation)
+    lettore = csv.reader(csvfile)
+    for riga in lettore:
+        anno = int(riga[0])
+        variazione_massa = int(riga[3])  # Considero solo la variazione cumulativa della massa
+        anni.append(anno)
+        variazione_massa_cumulativa.append(variazione_massa)
 
-# creazione del grafico
+# Creazione del grafico
 plt.figure(figsize=(10, 6))
-plt.plot(years, cumulative_mass_variation, marker='o', color='b', linestyle='-')
+plt.plot(anni, variazione_massa_cumulativa, marker='o', color='b', linestyle='-')
 plt.title('Variazione Cumulativa della Massa dell\'Aria nel Tempo')
 plt.xlabel('Anno')
 plt.ylabel('Variazione Cumulativa della Massa [m w.e. o 1000 kg m^-2]')
@@ -24,5 +23,5 @@ plt.grid(True)
 plt.xticks(rotation=45)
 plt.tight_layout()
 
-# mostra il grafico
+# Mostra il grafico
 plt.show()
